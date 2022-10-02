739dca5d3dccf60135528b95e68ba10382c1e2c9
William M <cominatyou@cominatyou.com>
2022-10-01 23:35:49 -0500

    Display dates for Tweets too far in the past

--- app/src/main/java/com/cominatyou/card/util/RelativeTimestamp.java
+++ app/src/main/java/com/cominatyou/card/util/RelativeTimestamp.java
@@ -1,6 +1,10 @@
 package com.cominatyou.card.util;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class RelativeTimestamp {
     public static String get(Instant instant) {
@@ -14,8 +18,30 @@ public class RelativeTimestamp {
         else if (seconds < 86400) {
             return seconds / 3600 + "h";
         }
-        else {
+        else if (seconds < 259200) {
             return seconds / 86400 + "d";
+        }
+        else {
+            final ZonedDateTime tweetTime = instant.atZone(ZoneId.systemDefault());
+            final ZonedDateTime now = ZonedDateTime.now(ZoneId.systemDefault());
+            final String month = DateTimeFormatter.ofPattern("MMM").format(tweetTime);
+
+            if (tweetTime.getYear() == now.getYear()) {
+                if (Locale.getDefault().equals(Locale.US)) {
+                    return month + " " + tweetTime.getDayOfMonth();
+                }
+                else {
+                    return tweetTime.getDayOfMonth() + " " + month;
+                }
+            }
+            else {
+                if (Locale.getDefault().equals(Locale.US)) {
+                    return month + " " + tweetTime.getDayOfMonth() + ", " + tweetTime.getYear();
+                }
+                else {
+                    return tweetTime.getDayOfMonth() + " " + month + " " + tweetTime.getYear();
+                }
+            }
         }
     }
 }
